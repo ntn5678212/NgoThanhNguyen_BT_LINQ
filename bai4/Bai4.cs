8f44fb788fbc6d65848e05c423efd4c2467f8ef3
ThanhNguyen <nguyen5678212@gmail.com>
2026-03-20 21:40:10 +0700

    nopbai4

---
+++ bai4/Bai4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Bai4
+{
+    static void Main()
+    {
+        Console.WriteLine("Ngo Thanh Nguyen_2415053122330_225LTC#02");
+        List<int> so = new List<int>() { 3, 4, 6, 8, 11, 4, 99, 7, 43, 1 };
+        var sol = so.OrderByDescending(x => x);
+        Console.WriteLine("cac so duoc sap xep tang dan la:");
+        foreach (int x in sol)
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
