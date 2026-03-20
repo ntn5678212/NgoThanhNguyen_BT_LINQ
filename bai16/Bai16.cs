483da467566883fd0476b41edb77d137b1c8b5dc
ThanhNguyen <nguyen5678212@gmail.com>
2026-03-20 22:17:18 +0700

    nopbai16

---
+++ bai16/Bai16.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BT_LINQ
+{
+    class Bai16
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ngo Thanh Nguyen_2415053122330_225LTC#02");
+            List<int> numbers = new List<int>() { 1, 2, 2, 3, 4, 4, 5,8,9 };
+            int so = numbers.Sum();
+            Console.WriteLine("tong cua danh sach la:");
+                Console.Write(so);
+
+
+        }
+    }
+}
