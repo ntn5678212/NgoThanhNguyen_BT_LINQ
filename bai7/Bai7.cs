6a433e581e5bdb143564be57f527c5eedc8e06b6
ThanhNguyen <nguyen5678212@gmail.com>
2026-03-20 22:08:47 +0700

    nopbai7

---
+++ bai7/Bai7.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BT_LINQ
+{
+    class Bai7
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ngo Thanh Nguyen_2415053122330_225LTC#02");
+            List<int> list = new List<int>() { 3,4,5,7,8,33};
+            bool kt=list.Any(x => x > 10);
+            Console.WriteLine(kt);
+        }
+    }
+}
