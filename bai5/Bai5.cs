59ad7f1afbb433517a79cbe26fbdfc8e536aea1e
ThanhNguyen <nguyen5678212@gmail.com>
2026-03-20 22:06:32 +0700

    nopbai5

---
+++ bai5/Bai5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BT_LINQ
+{
+    class bai5
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ngo Thanh Nguyen_2415053122330_225LTC#02");
+            List<int> numbers = new List<int> { 2, 3, 4, 5, 6 };
+            var so = numbers.Select(x => x * x);
+            Console.WriteLine("cac so sau khi binh phuong len la");
+            foreach (int x in so) 
+                Console.Write(x+" ");
+        }
+    }
+   
+
+}
