89eeb8eade5f0fd7f26af5194a1f742aa850d382
ThanhNguyen <nguyen5678212@gmail.com>
2026-03-20 22:24:00 +0700

    nopbai22

---
+++ bai22/Bai22.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace bai22
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string khoa { get; set; }
+        public double Score { get; set; }
+        public int Year { get; set; }
+    }
+
+    class Bai22
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ngo Thanh Nguyen_2415053122330_225LTC#02");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Random rd = new Random();
+
+            string[] names = { "An", "Binh", "Chi", "Dung", "Hieu", "Khanh", "Linh", "Minh" };
+            string[] khoas = { "CNTT", "KinhTe", "NgoaiNgu", "DienTu" };
+
+            List<Student> students = new List<Student>();
+
+
+            for (int i = 1; i <=10; i++)
+            {
+                students.Add(new Student
+                {
+                    Id = i,
+                    Name = names[rd.Next(names.Length)],
+                    Age = rd.Next(18, 25),
+                    khoa = khoas[rd.Next(khoas.Length)],
+                    Score = Math.Round(rd.NextDouble() * 10, 2),
+                    Year = rd.Next(1, 5)
+                });
+            }
+            Console.WriteLine("===== DANH SACH SINH VIEN =====\n");
+
+            foreach (var s in students)
+            {
+                Console.WriteLine($"ID: {s.Id,-3} | Ten: {s.Name,-6} | Tuoi: {s.Age,-2} | Khoa: {s.khoa,-10} | Diem: {s.Score,5}");
+            }
+            bool kt = students.Any(s => s.khoa == "CNTT");
+            if (kt)
+            {
+                Console.WriteLine("\nCo sinh vien thuoc khoa CNTT");
+            }
+            else
+            {
+                Console.WriteLine("\nKhong co sinh vien thuoc khoa CNTT");
+            }
+
+        }
+    }
+}
