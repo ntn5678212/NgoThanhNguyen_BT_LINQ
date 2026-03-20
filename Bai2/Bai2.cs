fc9e39ef7ec563d6270c41e527adccdbc22a92e0
ThanhNguyen <nguyen5678212@gmail.com>
2026-03-20 21:37:39 +0700

    nopbai2

---
+++ Bai2/Bai2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Bai2
+{
+    static void Main()
+    {
+        Console.WriteLine("Ngo Thanh Nguyen_2415053122330_225LTC#02");
+        List<int> so = new List<int>() { 3, 4, 6, 8, 11, 4, 99, 7, 43, 1 };
+        var sol = so.Where(x => x > 5);
+        Console.WriteLine("cac so lon hon 5 la");
+        foreach (int x in sol)
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
