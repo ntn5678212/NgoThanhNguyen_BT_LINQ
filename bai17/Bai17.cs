c7abe564538520439a54119d7af58543e894f2ad
ThanhNguyen <nguyen5678212@gmail.com>
2026-03-20 22:18:11 +0700

    nopbai17

---
+++ bai17/Bai17.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BT_LINQ
+{
+    class Bai17
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ngo Thanh Nguyen_2415053122330_225LTC#02");
+            List<int> numbers = new List<int>() { 1, 2, 2, 3, 4, 4, 5, 8, 9 };
+            double so = numbers.Average();
+            Console.WriteLine("trung binh cua danh sach la:");
+            Console.Write(so);
+
+
+        }
+    }
+}
